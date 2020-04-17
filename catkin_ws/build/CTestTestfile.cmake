f7f63bb157ff742cb6b9043e81508b6e43de97be
Natnael <natnael.abraha@colorado.edu>
2020-04-17 13:26:19 -0600

    turtlebot3

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/ROBOTICS/PROJECT/iRobot_Final_Project/catkin_ws/src
+# Build directory: /home/user/Desktop/ROBOTICS/PROJECT/iRobot_Final_Project/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
